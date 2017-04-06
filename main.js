b00f0611c0e2c8da101043c96e2c0c9698ac4c5c
carmenhaav <carmen.haav@khk.ee>
2017-04-06 21:48:33 +0300

    As a user I can remove the task - [Finishes #143250749]

--- main.js
+++ main.js
@@ -9,10 +9,19 @@ $(document).ready(function () {
         var newTask = $('#addList').val();
 
         if (newTask.length>0){
-            $('#toDoList').append('<tr><td><input type="checkbox"><input id="showList" type="text" value="' + newTask + '"></td></tr>');
+            $('#toDoList').append('<tr><td><input id="checkList" type="checkbox"><input id="showList" type="text" value="' + newTask + '"></td></tr>');
 
             $('#addList').val('');
         }
+   });
+
+   // remove a list item/task
+   $('#remove').click(function () {
+      $('td').find('input').each(function () {
+         if ($(this).is(':checked')){
+             $(this).parents('td').remove();
+         }
+      });
    });
 
 });
