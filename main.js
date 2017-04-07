629bbe9203496020a8b6815f0501f43807d7f63f
carmenhaav <carmen.haav@khk.ee>
2017-04-07 16:25:44 +0300

    Major fixes - disabled directories browsing, added ajax & fixed design

--- main.js
+++ main.js
@@ -3,26 +3,45 @@
  */
 
 $(document).ready(function () {
-
+    var id = 0;
     // add a new list item/task
-   $('#add').click(function () {
+    $('#add').click(function () {
+        id++;
         var newTask = $('#addList').val();
 
-        if (newTask.length>0){
-            $('#toDoList').append('<tr><td><input id="checkList" type="checkbox"><input id="showList" type="text" value="' + newTask + '"></td></tr>');
+        $.ajax({
+            method: "POST",
+            data: {
+                row_id: id,
+                new_task: newTask
+            }
+        });
+
+        if (newTask.length > 0) {
+            $('table tbody').append('<tr><td><input id="' + id + '" type="checkbox" name="checkTick"></td><td><input class="form-control" id="showList" type="text" value="' + newTask + '"></td></tr>');
 
             $('#addList').val('');
         }
-   });
+    });
 
-   // remove a list item/task
-   $('#remove').click(function () {
-      $('td').find('input').each(function () {
-         if ($(this).is(':checked')){
-             $(this).parents('td').remove();
-         }
-      });
-   });
+    // remove a list item/task
+    $('#remove').click(function () {
+        var row_id = $("input:checked").prop('id');
+
+        $.ajax({
+            method: "GET",
+            data: {
+                "id": row_id
+            }
+        });
+
+        $('table tbody').find('input[name="checkTick"]').each(function () {
+            if ($(this).is(':checked')) {
+                $(this).parents('tr').remove();
+            }
+        });
+        id--;
+    });
 
 });
 
