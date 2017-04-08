d6ef2ec5323256a071ba54a77aa21f3da68825cb
carmenhaav <carmen.haav@khk.ee>
2017-04-08 19:25:47 +0300

    Major design fixes, structured directories and files, added comments.

--- assets/main.js
+++ assets/main.js
@@ -3,12 +3,16 @@
  */
 
 $(document).ready(function () {
+
     var id = 0;
+
     // add a new list item/task
     $('#add').click(function () {
+
         id++;
         var newTask = $('#addList').val();
 
+        // ajax POST method
         $.ajax({
             method: "POST",
             data: {
@@ -17,17 +21,22 @@ $(document).ready(function () {
             }
         });
 
+        // allows to insert content as a last child
         if (newTask.length > 0) {
-            $('table tbody').append('<tr><td><input id="' + id + '" type="checkbox" name="checkTick"></td><td><input class="form-control" id="showList" type="text" value="' + newTask + '"></td></tr>');
+            $('table tbody').append('<tr><td><input id="' + id + '" type="checkbox" name="checkTick"><label for="' + id + '"></label></td><td><input class="form-control" id="showList" type="text" value="' + newTask + '"></td></tr>');
 
+            // leaves an empty value to the input box after click event
             $('#addList').val('');
         }
     });
 
     // remove a list item/task
     $('#remove').click(function () {
+
+        // gets the row id
         var row_id = $("input:checked").prop('id');
 
+        // ajax GET method
         $.ajax({
             method: "GET",
             data: {
@@ -35,6 +44,7 @@ $(document).ready(function () {
             }
         });
 
+        // if checked, then remove row
         $('table tbody').find('input[name="checkTick"]').each(function () {
             if ($(this).is(':checked')) {
                 $(this).parents('tr').remove();
